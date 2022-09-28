7965f8c1771a6c589221d1c5289529455665bc7b
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2022-09-28 18:19:55 +0900

    Write System spec with FactoryBot

--- spec/system/book_spec.rb
+++ spec/system/book_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe "books", type: :system do
   it "GET /books" do
-    visit "/books"
-    expect(page).to have_text("Books")
+    book = FactoryBot.create(:book)
+    visit "/books" # /booksへHTTPメソッドGETでアクセス
+    expect(page).to have_text(book.title)
   end
 
   it "POST /books" do
