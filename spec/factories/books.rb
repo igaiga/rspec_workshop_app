28e6827f95d4ff60de03644d0d935da3d09a422f
Kuniaki Igarashi <kuniaki.igarashi@garnettech373.com>
2023-06-23 16:17:48 +0900

    Fix trait on FactoryBot

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :book do # :book はモデル名を全小文字にしてシンボルにしたもの
     title { "RubyBook" } # カラム title のデータとして "RubyBook" をつかう
     author { "matz" } # カラム author のデータとして "matz" をつかう
-  end
 
-  trait :with_variations do
-    after(:create) do |book|
-      book.variations.create!(kind: "paper book")
+    trait :with_variations do
+      after(:create) do |book|
+        book.variations.create!(kind: "paper book")
+      end
     end
   end
 end
